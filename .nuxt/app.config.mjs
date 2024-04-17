9134b3b0a988dc2b29248404cba112c0caac34ff
Brandon Bondig <brandon@bondig.dk>
2024-04-17 19:25:00 +0200

    Upload unzipped files from S3

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,20 @@
+
+import { updateAppConfig } from '#app/config'
+import { defuFn } from 'defu'
+
+const inlineConfig = {
+  "nuxt": {
+    "buildId": "e392b35c-0ed4-4e87-bcc5-c85ab89f3409"
+  }
+}
+
+// Vite - webpack is handled directly in #app/config
+if (import.meta.hot) {
+  import.meta.hot.accept((newModule) => {
+    updateAppConfig(newModule.default)
+  })
+}
+
+
+
+export default /*@__PURE__*/ defuFn(inlineConfig)
